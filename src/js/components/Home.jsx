cd72c93b0ae098b773f8ba60f0c6dc053641fb09
Alejandro Guzmán <j.alejandro.guzman.m@gmail.com>
2025-05-14 00:10:50 +0000

    implementando funcionalidad con fetch, terminado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,133 +1,44 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const Home = () => {
 
-	const dominio = "https://playground.4geeks.com"
-	const songs = [{
-		id: 1,
-		name: "Mario Castle",
-		url: "/sound/files/mario/songs/castle.mp3",
-		category: "category"
-	},
-	{
-		id: 2,
-		name: "Mario Star",
-		url: "/sound/files/mario/songs/hurry-starman.mp3",
-		category: "category"
-	},
-	{
-		id: 3,
-		name: "Mario Overworld",
-		url: "/sound/files/mario/songs/overworld.mp3",
-		category: "category"
-	},
-	{
-		id: 4,
-		name: "Mario Stage 1",
-		url: "/sound/files/mario/songs/stage1.mp3",
-		category: "category"
-	},
-	{
-		id: 5,
-		name: "Mario Stage 2",
-		url: "/sound/files/mario/songs/stage2.mp3",
-		category: "category"
-	},
-	{
-		id: 6,
-		name: "Mario Star",
-		url: "/sound/files/mario/songs/starman.mp3",
-		category: "category"
-	},
-	{
-		id: 7,
-		name: "Mario Underworld",
-		url: "/sound/files/mario/songs/underworld.mp3",
-		category: "category"
-	},
-	{
-		id: 8,
-		name: "Mario Underwater",
-		url: "/sound/files/mario/songs/underwater.mp3",
-		category: "category"
-	},
-	{
-		id: 9,
-		name: "Zelda Castle",
-		url: "/sound/files/videogame/songs/zelda_castle.mp3",
-		category: "category"
-	},
-	{
-		id: 10,
-		name: "Zelda Outworld",
-		url: "/sound/files/videogame/songs/zelda_outworld.mp3",
-		category: "category"
-	},
-	{
-		id: 11,
-		name: "Zelda Titles",
-		url: "/sound/files/videogame/songs/zelda_title.mp3",
-		category: "category"
-	},
-	{
-		id: 12,
-		name: "Sonic Brain Zone",
-		url: "/sound/files/videogame/songs/sonic_brain-zone.mp3",
-		category: "category"
-	},
-	{
-		id: 13,
-		name: "Zelda Link To Past",
-		url: "/sound/files/videogame/songs/zelda_link-to-past.mp3",
-		category: "category"
-	},
-	{
-		id: 14,
-		name: "Flintstones",
-		url: "/sound/files/cartoons/songs/flintstones.mp3",
-		category: "cartoon"
-	},
-	{
-		id: 15,
-		name: "power-rangers",
-		url: "/sound/files/cartoons/songs/power-rangers.mp3",
-		category: "cartoon"
-	},
-	{
-		id: 16,
-		name: "simpsons",
-		url: "/sound/files/cartoons/songs/simpsons.mp3",
-		category: "cartoon"
-	},
-	{
-		id: 17,
-		name: "south-park",
-		url: "/sound/files/cartoons/songs/south-park.mp3",
-		category: "cartoon"
-	},
-	{
-		id: 18,
-		name: "thundercats",
-		url: "/sound/files/cartoons/songs/thundercats.mp3",
-		category: "cartoon"
-	},
-	{
-		id: 19,
-		name: "x-men",
-		url: "/sound/files/cartoons/songs/x-men.mp3",
-		category: "cartoon"
-	}
-	]
-	const audioRef = useRef(null);
+	const [songs, setSongs] = useState([])
 	const [currentSong, setCurrentSong] = useState(null);
+	const [error, setError] = useState(null);
 	const [isPlaying, setIsPlaying] = useState(false);
-	const [currentIndex, setCurrentIndex] = useState(0)
+	const [currentIndex, setCurrentIndex] = useState(0);
+
+	const audioRef = useRef(null);
+
+	useEffect(() => {
+		findSong()
+	}, [])
+
+	async function findSong() {
+		try {
+			const response = await fetch('https://playground.4geeks.com/sound/songs', {
+				method: 'GET',
+				headers: {
+					'Content-type': 'application/json',
+				}
+			})
+
+			if (response.status === 404) throw Error("Página no encontrada...")
+			const responseJson = await response.json();
+			console.log('Canciones recibidas:', responseJson.songs);
+			setSongs(responseJson.songs);
+
+		} catch (error) {
+			console.error('Error fetching songs:', error);
+			setError(error.message);
+		}
+	}
 
 	const playSong = (song, index) => {
-		const fullUrl = dominio + song.url;
 		setCurrentSong(song);
 		setCurrentIndex(index);
 		setIsPlaying(true);
+		const fullUrl = `https://playground.4geeks.com${song.url}`;
 		audioRef.current.src = fullUrl;
 		audioRef.current.play();
 	};
@@ -153,22 +64,34 @@ const Home = () => {
 		setIsPlaying(!isPlaying);
 	};
 
+
+
 	return (
 		<div className="contenedor">
+			{
+				error !== null && (
+					<div className="alert alert-warning" role="alert">
+						{error}
+					</div>
+				)
+			}
 			<h1>Audio Player</h1>
 			<ul>
-				{songs.map((song, index) => (
-					<li key={song.id}>
-						<button onClick={() => playSong(song, index)}
-							className={currentSong?.id === song.id ? 'active' : ''}>{song.name}</button>
-					</li>
-				))}
+				{
+					songs.length > 0 &&
+					songs.map((song, index) => (
+						<li key={song.id}>
+							<button onClick={() => playSong(song, index)}
+								className={currentSong?.id === song.id ? 'active' : ''}>{song.name}</button>
+						</li>
+					))
+				}
 			</ul>
 			<audio ref={audioRef} onEnded={playNext} />
 			<div className="controls">
-				<button onClick={playPrevious}><i class="fa-solid fa-backward"></i></button>
-				<button onClick={togglePlayPause}>{isPlaying ? <i class="fa-solid fa-pause"></i> : <i class="fa-solid fa-play"></i>}</button>
-				<button onClick={playNext}><i class="fa-solid fa-forward"></i></button>
+				<button onClick={playPrevious}><i className="fa-solid fa-backward"></i></button>
+				<button onClick={togglePlayPause}>{isPlaying ? <i className="fa-solid fa-pause"></i> : <i className="fa-solid fa-play"></i>}</button>
+				<button onClick={playNext}><i className="fa-solid fa-forward"></i></button>
 			</div>
 		</div>);
 };
